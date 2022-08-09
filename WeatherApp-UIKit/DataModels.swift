4ad2706046cfea149cbc3a794ad3f65a735917ef
SIM0N0V <105148218+SIM0N0V@users.noreply.github.com>
2022-08-09 23:36:48 +0400

    Start project

---
+++ WeatherApp-UIKit/DataModels.swift
@@ -0,0 +1,8 @@
+//
+//  DataModels.swift
+//  WeatherApp-UIKit
+//
+//  Created by SIMONOV on 09.08.2022.
+//
+
+import Foundation
--- WeatherApp-UIKit/ViewController.swift
+++ WeatherApp-UIKit/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       startLocationManager()
+    }
+    private func startLocationManager() {
+        locationManager.requestWhenInUseAuthorization() /
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.pausesLocationUpdatesAutomatically = false
+            locationManager.startUpdatingLocation() 
+        }
     }
 
-
+}
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            print(lastLocation.coordinate.latitude, lastLocation.coordinate.longitude)
+        }
+    }
 }
 
